93e98f04b21294b60812585e9baa7d0d3f89b730
jacob cargen <jacobcargen@gmail.com>
2023-01-05 13:20:19 -0500

    Add project files.

---
+++ SelfDestructController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Self_Destruct_Mod
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class SelfDestructController
+    {
+        private bool selfDestruct;
+
+        public void Start()
+        {
+            Debug.Log("Self Destruct Ready!");
+        }
+        public void Update()
+        {
+            // If player tries to self destruct
+            selfDestruct = Input.GetKeyDown(KeyCode.Backspace) && Input.GetKey(KeyCode.LeftControl)
+                && Input.GetKey(KeyCode.RightControl);
+            // Self destruct the ship
+            if (selfDestruct) SelfDestruct();
+        }
+        private void SelfDestruct()
+        {
+            Vessel ves = FlightGlobals.ActiveVessel;
+            Debug.Log("Self Destruct!");
+        }
+    }
+}
